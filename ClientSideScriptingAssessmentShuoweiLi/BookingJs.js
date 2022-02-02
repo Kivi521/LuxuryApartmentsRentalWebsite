a8899374d1f2372b3cf3cf36e030cb00fe72ff18
Kivi521 <87478165+Kivi521@users.noreply.github.com>
2022-02-03 10:28:43 +1100

    init commit

---
+++ ClientSideScriptingAssessmentShuoweiLi/BookingJs.js
@@ -0,0 +1,186 @@
+﻿/** Instantiate date */
+
+var date = new Date();
+
+/** set firs day and last day which allowed choose for customer */
+
+var firstDay = new Date(date.getFullYear(), date.getMonth(), 1);//1-Nov-2021
+
+var lastDay = new Date(date.getFullYear(), date.getMonth() + 2, 31);//31-Jan-2022
+
+
+while (firstDay <= lastDay) {
+    var opt = document.createElement("option");/** this is an optional element: <option></option> */
+
+    var text = document.createTextNode(firstDay);/** 1-Nov-2021 */
+
+    opt.appendChild(text); /** <option>1-Nov-2021</option> */ 
+
+    document.getElementById("myDropdown").appendChild(opt);/** <select><option>1-Nov-2021</option></select> */
+
+    firstDay.setDate(firstDay.getDate() + 1)/** set first day */
+}
+
+/** Class representing date and price.
+ * @constructor
+ */
+
+class DatePrice {
+    constructor(d) { /** d - string date */
+
+        this.d = new Date(d);/**  this.d - date object */
+
+        this.p = this.getPrice();/**  $270 or &250 */
+        
+
+    }
+   /** getPrice method is for distinguishing different price for different dates */
+
+    getPrice() {
+        let renewalDate = new Date(2021, 11, 18);
+        console.log(this.d);
+        console.log(renewalDate)
+        console.log(this.d <= renewalDate);
+        if (this.d <= renewalDate) { /**  1-Nov-2o21 --- 18-Dec-2021 fee is: $270 per night */
+
+            return 270;
+        }
+        else { /**  18-Dec-2021 --- 31-Jan-2022 fee is $250 per night */
+
+            return 250;
+        }
+        
+    }
+}
+/** Array selectedDates to store selected dates */
+
+let selectedDates = new Array();
+
+/** @founction addDate
+ * is for adding selected by the customer to selectedDates array */
+
+function addDate() {
+
+/** get the date selected from myDropdown selecet element */
+
+    let selectedD = document.getElementById("myDropdown").value;
+/** add the selected date as a DatePrice obj to the array */
+
+    selectedDates.push(new DatePrice(selectedD));
+
+    selectedDates.sort(compare);
+
+
+}
+
+/**
+ * Function to sort the date by order
+ * Function compare
+ * @param {string} a - date a;
+ * @param {string} b - date b
+ * @returns comparison
+ */
+
+function compare(a, b) {
+    const dateA = a.d;
+    const dateB = b.d;
+
+    let comparison = 0;
+    if (dateA > dateB) {
+        comparison = 1;
+    } else if (dateA < dateB) {
+        comparison = -1;
+    }
+    return comparison;
+}
+
+
+/** @ function displayDates,
+ * for achieveing this function: when press display button, the montor will show total price */
+
+function displayDates() {
+    let total = 0;
+    document.getElementById("myDropdown2").innerHTML = "";
+    for (var i = 0; i < selectedDates.length; i++) {
+        var opt = document.createElement("option");
+        var text = document.createTextNode(selectedDates[i].d);
+        opt.appendChild(text);
+        document.getElementById("myDropdown2").appendChild(opt);
+        total += selectedDates[i].p;
+    }
+    document.getElementById("demo").innerHTML = total;
+    //console.log(selectedDates);
+}
+
+
+/**
+ * @Function binarySearch
+ * to do the binary search of selected dates and delete the date
+ * Function binarySearch
+ * @param {string} date - The date to be deleted
+ * @param {list} list - the list of selected dates
+ * @return position
+ */
+
+function binarySearch(dateStr, list) {
+    let first = 0;
+    let last = list.length - 1;
+    let position = -1;
+    let found = false;
+    let middle;
+    let selectedDate = new Date(dateStr);
+    console.log(typeof (dateStr));
+    console.log(selectedDate.getTime());
+    console.log(list[0].d.getTime());
+    while (found === false && first <= last) {
+        middle = Math.floor((first + last) / 2);
+        console.log(middle);
+        console.log(list[middle]);
+        console.log(list[middle].d.getTime() === selectedDate.getTime());
+        if (list[middle].d.getTime() === selectedDate.getTime()) { 
+            found = true;
+            position = middle;
+        }
+        else if (list[middle].d.getTime() > selectedDate.getTime()) {
+            last = middle - 1;
+        }
+        else {
+            first = middle + 1;
+        }
+    }
+    return position;
+}
+
+
+/**@function removeDate
+ * for achieveing this function by binarySearch function: when press remove button, the selected date will be removed
+ * */
+
+function removeDate() {
+
+    //get the position/index of selected item in myDropdown2
+
+    //let index = document.getElementById("myDropdown2").selectedIndex;
+
+    //update the total price
+
+    let select  = document.getElementById("myDropdown2");
+    
+    var dateValue = select.options[select.selectedIndex].value;
+    
+    let posFound = binarySearch(dateValue, selectedDates);
+    
+    console.log(posFound);
+    let currTotal = document.getElementById("demo").innerHTML;
+    currTotal -= selectedDates[posFound].p;
+    document.getElementById("demo").innerHTML = currTotal;
+
+/** remove the date from array */
+
+    selectedDates.splice(posFound, 1);
+
+/** remove the date from myDropdown2 */
+
+    select.remove(posFound);
+
+}
---
+++ ClientSideScriptingAssessmentShuoweiLi/ValidateJs.js
@@ -0,0 +1,106 @@
+﻿
+
+/** call the functions */
+
+document.getElementById("contactForm").onsubmit = function () {
+    let isValidName = validateName();
+    let isValidPhone = validatePhone();
+    let isValidEmail = validateEmail();
+    let isValidGender = validateGender();
+    let isValidLeaveMes = validateLeaveMes();
+    
+    return isValidName && (isValidPhone || isValidEmail) && isValidGender && isValidLeaveMes;
+}
+
+
+/** validate name input value */
+
+function validateName() {
+    let x = document.getElementById("txtName").value;
+    /** valid - Username field must contain at least two letters (uppercase and lowercase)*/
+
+    if (/[a-z]/.test(x) && /[A-Z]/.test(x)) {
+        document.getElementById("txtName_Help").innerText = "";
+        return true;
+    }
+    else {
+        /** invalid;*/
+
+        document.getElementById("txtName_Help").innerText = "Name must contain at least one lowercase and one uppercase characters";
+        return false;
+    }
+}
+/** validate phone input value */
+
+function validatePhone() {
+    /** Phone field must contain digits only */
+
+    let x = document.getElementById("txtPhone").value;
+    if (x === "" || isNaN(x)) {
+        /** invalid - missing value or non numeric value */
+        document.getElementById("txtPhone_Help").innerText = "phone must contain digits only";
+        return false;
+    }
+    else {
+        /** valid */
+
+        document.getElementById("txtPhone_Help").innerText = "";
+        return true;
+    }
+
+}
+
+/** validate email input value */
+
+function validateEmail() {
+    /** Email field must contain a valid email format */
+
+    let x = document.getElementById("txtEmail").value;
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(x)) {
+        return true;
+    }
+
+    else {
+        /** valid */
+
+        document.getElementById("txtEmail_Help").innerText = "Email field must contain a valid email format";
+        return false;
+    }
+
+}
+
+/** validate gender input value */
+
+function validateGender() { 
+    /** Gender must not be null */
+
+    let x = document.getElementById("txtGender").value;
+    if (x === "") {
+        document.getElementById("txtGender_Help").innerText = "Gender field cannot be left empty";
+        return false;
+    }
+    else {
+        document.getElementById("txtGender_Help").innerText = "";
+        return true;
+    }
+}
+
+
+/** validate massage input value */
+
+function validateLeaveMes() {
+    /** Message field cannot be left empty */
+
+    let x = document.getElementById("txtLeaveMes").value;
+    console.log(x);
+    console.log(x === "");
+    if (x === "") {
+        document.getElementById("txtLeaveMes_Help").innerText = "Message field cannot be left empty";
+        return false;
+    }
+    else {/** valid */
+
+        document.getElementById("txtLeaveMes_Help").innerText = "";
+        return true;
+    }
+}
